1a5e250cd84540b0849d684a293558b517b90884
harshjain2309 <jainh2113@gmail.com>
2025-01-27 20:19:04 +0530

    data transformation

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
@@ -32,3 +33,17 @@ def read_sql_data():
 
     except Exception as ex:
         raise CustomException(ex,sys)
+    
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
